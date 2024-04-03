bffd3bd1c8a0abcf4e1c4363721f99eb89772e4e
G4GA <angel.garcia9106@alumnos.udg.mx>
2024-04-03 00:18:24 -0600

    feat: add Piece class method declarations
    in here i declare the methods for the Piece class
    (i may add more latter on)

--- include/pieces.hpp
+++ include/pieces.hpp
@@ -7,9 +7,19 @@ An abstract class for pieces
 class Piece
 {
     public:
-        Piece ();
+        Piece();
+        Piece(const std::string&);
+        Piece(const Piece&);
+        sf::Vector2f getPosition() const;
+        void setPosition(const sf::Vector2f&);
+        void setPosition(const float&, const float&);
+        void loadFile(const std::string&);
+        sf::Sprite getStripe() const;
     private:
         sf::Image i_piece;
+        sf::Texture i_texture;
+        sf::Sprite i_sprite;
+        sf::Vector2f position;
 };
 
 class Pawn : Piece
