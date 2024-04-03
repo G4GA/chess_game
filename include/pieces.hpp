b05840c8a51f4d553aef42f2570794898b35c3d9
G4GA <angel.garcia9106@alumnos.udg.mx>
2024-04-03 00:37:35 -0600

    feat: adding some getters and setters for the Piece class
    specifically, the one addded where for:
            - the position
            - the size
            - the sprite object
    for the position and the size object i also added some overloads
    for supporting the sf::Vector2f class, as well as float value

--- include/pieces.hpp
+++ include/pieces.hpp
@@ -9,17 +9,24 @@ class Piece
     public:
         Piece();
         Piece(const std::string&);
+        Piece(const std::string&, const sf::Vector2f&);
         Piece(const Piece&);
         sf::Vector2f getPosition() const;
         void setPosition(const sf::Vector2f&);
         void setPosition(const float&, const float&);
-        void loadFile(const std::string&);
+        void setSize (const float&, const float&);
+        void setScaleFactor(const float&, const float&);
+        void setScaleFactor(const sf::Vector2f&);
+        sf::Vector2f getScaleFactor () const;
+        sf::Vector2f getSize() const;
         sf::Sprite getStripe() const;
+        bool loadFile(const std::string&);
     private:
         sf::Image i_piece;
         sf::Texture i_texture;
         sf::Sprite i_sprite;
         sf::Vector2f position;
+        sf::Vector2f size;
 };
 
 class Pawn : Piece
